eeec79972bb43f185d8203cde377b5015509468a
VadymLeus <ipz223_lvo@student.ztu.edu.ua>
2026-03-06 16:59:24 +0200

    Init lab3: created base project and configured App.js for gestures

---
+++ lab3/App.js
@@ -0,0 +1,9 @@
+import 'react-native-gesture-handler';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+    </GestureHandlerRootView>
+  );
+}
